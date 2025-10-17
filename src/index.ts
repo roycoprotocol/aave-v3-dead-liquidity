e1d0be3a8618304c141c3d3ecd38685b4daad1af
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2025-10-16 22:25:42 -0700

    add weth

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,8 @@ const TIME_IDLE_SECONDS_AGO = Math.floor(Date.now() / 1000) - IDLE_SECONDS;
 const TOKEN_MAP = new Map([
     ["0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", "USDC"], // Mainnet USDC
     ["0xdac17f958d2ee523a2206206994597c13d831ec7", "USDT"], // Mainnet USDT  
-    ["0x6b175474e89094c44da98b954eedeac495271d0f", "DAI"]   // Mainnet DAI
+    ["0x6b175474e89094c44da98b954eedeac495271d0f", "DAI"],  // Mainnet DAI
+    ["0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", "WETH"] // Mainnet WETH
 ]);
 
 async function main() {
